ea701392a2a3d366389841a9886f063464272767
Carles Andres <carlos.andres.c@gmail.com>
2024-04-03 09:48:51 +0100

    Added opengraph-image for about route

---
+++ app/about/opengraph-image.tsx
@@ -0,0 +1,81 @@
+import { ImageResponse } from 'next/og';
+
+// Route segment config
+export const runtime = 'edge';
+
+// Image metadata
+export const alt = 'Git Example';
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = 'image/png';
+
+export default async function Image() {
+
+  const geistMonoVariable = fetch(
+    new URL('../../assets/GeistMonoVF.ttf', import.meta.url),
+  ).then((res) => res.arrayBuffer());
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontFamily: 'Geist',
+          fontSize: 128,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <div
+          style={{
+            fontSize: 96,
+            fontWeight: 'bold',
+            background: 'white',
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'blue',
+            padding: '0 32px',
+          }}
+        >
+          Title
+        </div>
+        <div
+          style={{
+            fontSize: 64,
+            background: 'white',
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            padding: '0 32px',
+          }}
+        >
+          Subtitle
+        </div>
+      </div>
+    ),
+    {
+      ...size,
+      fonts: [
+        {
+          name: 'Geist',
+          data: await geistMonoVariable,
+          style: 'normal',
+          weight: 400,
+        },
+      ],
+    },
+  );
+}
+
